f78683f495e3dd3362254847d7fd89ac253eb59f
Tanvirmehedi <tjmehedi201@gmail.com>
2023-10-03 09:48:04 +0600

    work page filtaring added

--- src/pages/Resume/SkillServices.jsx
+++ src/pages/Resume/SkillServices.jsx
@@ -10,7 +10,7 @@ export const SkillServices = ({ data }) => {
       </h2>
       <div className="grid grid-flow-row-dense grid-cols-2 grid-rows-2 gap-2 mt-3 px-2">
         {list.map((item, index) => (
-          <span className="bg-[#E1E8EF] text-slate-700 font-thin rounded-md p-2 flex items-center justify-center shadow-sm shadow-gray-400">{item}</span>
+          <span key={index} className="bg-[#E1E8EF] text-slate-700 font-thin rounded-md p-2 flex items-center justify-center shadow-sm shadow-gray-400">{item}</span>
         ))}
       </div>
     </div>
