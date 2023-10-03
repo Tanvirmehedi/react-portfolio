c7bdf058c500cbf895847d19dd6a2bf345fbf290
Tanvirmehedi <tjmehedi201@gmail.com>
2023-10-03 06:58:16 +0600

    work page filtaring added

--- src/components/ProfileDetails.jsx
+++ src/components/ProfileDetails.jsx
@@ -3,7 +3,7 @@ import { NameCard } from "./NameCard";
 
 export function ProfileDetails() {
     return (
-      <section className="profileDetails bg-white rounded-md p-3 lg:px-16 m-auto w-full text-center lg:col-span-2 mt-16 lg:mt-24">
+      <section className="profileDetails bg-white rounded-md p-3 lg:px-6 m-auto w-full text-center lg:col-span-2 mt-16 lg:mt-24">
         <NameCard />
         <InfoCard />
       </section>
