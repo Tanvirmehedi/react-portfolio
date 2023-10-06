d33f352d33b02e3b7e8cfea7ae507163dc5bb6dc
Tanvirmehedi <tjmehedi201@gmail.com>
2023-10-06 07:22:28 +0600

    responsiveness improve

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 export function MyApp() {
   return (
     <div>
-      <motion.main className="portfolio-main pt-6 mx-auto bg-[#F2F5F9] flex flex-col gap-4 font-[railway] md:min-h-screen md:grid md:grid-cols-5 md:gap-1 md:content-start ">
+      <motion.main className="portfolio-main pt-6 mx-auto bg-[#F2F5F9] flex flex-col gap-4 font-[railway] md:min-h-screen md:grid md:grid-cols-5 md:gap-1 md:content-start">
         <ProfileDetails />
         <AboutSection />
       </motion.main>
--- src/components/NameCard.jsx
+++ src/components/NameCard.jsx
@@ -1,13 +1,11 @@
 /** @format */
 
 import { motion } from "framer-motion";
+import profileImage from "../assets/IMG-20210116-WA002fgggggggggggggggggggggggggg6.png.jpg";
 export function NameCard() {
   return (
     <div className="name-card flex flex-col justify-center items-center gap-2">
-      <div
-        className="profile-image rounded-lg w-32 h-32 overflow-hidden -mt-16 md:-mt-24"
-        cx={500}
-      >
+      <div className="profile-image rounded-lg w-32 h-32 overflow-hidden -mt-16 md:-mt-24">
         <motion.img
           animate={{ x: [-100, 0, 0] }}
           transition={{ ease: "easeInOut", duration: 1.5 }}
