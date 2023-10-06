9ea3dd7181ca1d0b6b62d41de39859410c45a61e
Tanvirmehedi <tjmehedi201@gmail.com>
2023-10-06 07:05:51 +0600

    responsiveness improve

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,20 @@
+/** @format */
+import { motion } from "framer-motion";
 export function Button({ href, icon, text }) {
-    return (
-      <div className="resume mt-6 m-auto flex flex-row justify-center items-center">
-        <a href={href} target="_blank" rel="noopener noreferrer">
-          <button className="resume-btn hover:animate-pulse rounded-full cursor-pointer p-3 text-xl flex flex-row gap-x-2 justify-center items-center bg-gradient-to-r from-[#1BBAD6] to-[#FFCE42] text-white">
-            <div className="icon -mt-1">{icon}</div>
-            <div className="btn-text text-sm md:text-base font-semibold">{text}</div>
-          </button>
-        </a>
-      </div>
-    );
-  }
+  return (
+    <div className="resume mt-6 m-auto flex flex-row justify-center items-center">
+      <a href={href} target="_blank" rel="noopener noreferrer">
+        <motion.button
+          className="resume-btn hover:animate-pulse rounded-full cursor-pointer p-3 text-xl flex flex-row gap-x-2 justify-center items-center bg-gradient-to-r from-[#1BBAD6] to-[#FFCE42] text-white"
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+        >
+          <div className="icon -mt-1">{icon}</div>
+          <div className="btn-text text-sm md:text-base font-semibold">
+            {text}
+          </div>
+        </motion.button>
+      </a>
+    </div>
+  );
+}
--- src/components/NameCard.jsx
+++ src/components/NameCard.jsx
@@ -1,20 +1,27 @@
-import profileImage from "../assets/IMG-20210116-WA002fgggggggggggggggggggggggggg6.png.jpg"
+/** @format */
+
+import { motion } from "framer-motion";
 export function NameCard() {
-    return (
-      <div className="name-card flex flex-col justify-center items-center gap-2">
-        <div className="profile-image rounded-lg w-32 h-32 overflow-hidden -mt-16 md:-mt-24">
-          <img
-            className="max-h-fit"
-            src="https://tanvirmehedi.github.io/react-portfolio-live/main.jpg"
-            alt="profile-img"
-          />
-        </div>
-        <div className="profile-name text-2xl font-semibold">
-          H M Tanvir Mehedi
-        </div>
-        <div className="profile-designation font-thin text-sm text-gray-400">
-          FullStack Developer
-        </div>
+  return (
+    <div className="name-card flex flex-col justify-center items-center gap-2">
+      <div
+        className="profile-image rounded-lg w-32 h-32 overflow-hidden -mt-16 md:-mt-24"
+        cx={500}
+      >
+        <motion.img
+          animate={{ x: [-100, 0, 0] }}
+          transition={{ ease: "easeInOut", duration: 1.5 }}
+          className="max-h-fit"
+          src="https://tanvirmehedi.github.io/react-portfolio-live/main.jpg"
+          alt="profile-img"
+        />
       </div>
-    );
-  }
+      <div className="profile-name text-2xl font-semibold">
+        H M Tanvir Mehedi
+      </div>
+      <div className="profile-designation font-thin text-sm text-gray-400">
+        FullStack Developer
+      </div>
+    </div>
+  );
+}
